ab1bb0404838b6e86e28b0a7bc8649ab2c27a36d
Yan Lyra <yanlyra3@gmail.com>
2022-10-12 02:37:59 -0300

    feat: finished input component

--- src/components/TextInput/TextInput.stories.tsx
+++ src/components/TextInput/TextInput.stories.tsx
@@ -1,13 +1,30 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { TextInput, TextInputProps } from ".";
+import { Envelope } from "phosphor-react";
+import { TextInput, TextInputRootProps } from ".";
 
 export default {
   title: "Components/TextInput",
   component: TextInput,
   args: {
-    placeholder: "Type your data",
+    children: <TextInput.Input placeholder="Type your data" />,
   },
-  argTypes: {},
-} as Meta<TextInputProps>;
+  argTypes: {
+    children: {
+      control: {
+        disable: true,
+      },
+    },
+  },
+} as Meta<TextInputRootProps>;
 
-export const Default: StoryObj<TextInputProps> = {};
+export const Default: StoryObj<TextInputRootProps> = {};
+export const WithIcon: StoryObj<TextInputRootProps> = {
+  args: {
+    children: [
+      <TextInput.Icon>
+        <Envelope />
+      </TextInput.Icon>,
+      <TextInput.Input placeholder="Type your data" />,
+    ],
+  },
+};
--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,14 +1,38 @@
-import { InputHTMLAttributes } from "react";
+import { cloneElement, InputHTMLAttributes, ReactNode } from "react";
 
-export type TextInputProps = InputHTMLAttributes<HTMLInputElement> & {};
+export type TextInputRootProps = {
+  children: ReactNode;
+};
 
-export function TextInput({ className, ...props }: TextInputProps) {
+export function TextInput({ children }: TextInputRootProps) {
   return (
-    <div className="border-none outline-none focus:ring-2 ring-offset-2 ring-offset-gray-800 focus:ring-cyan-500 py-4 px-3 rounded  text-gray-100 text-sm placeholder:text-gray-400 w-full bg-gray-800">
-      <input
-        className="bg-[transparent] w-full border-none outline-none"
-        {...props}
-      />
+    <div className="border-none outline-none focus:ring-2 ring-offset-2 ring-offset-gray-800 focus:ring-cyan-500 py-4 px-3 rounded w-full bg-gray-800 flex itens-center gap-3">
+      {children}
     </div>
   );
 }
+
+export type TextInputIconProps = {
+  children: JSX.Element;
+};
+
+function TextInputIcon({ children }: TextInputIconProps) {
+  return cloneElement(children, {
+    className: "w-6 h-6 text-gray-400",
+  });
+}
+TextInputIcon.displayName = "TextInput.Icon";
+
+export type TextInputInputProps = InputHTMLAttributes<HTMLInputElement> & {};
+function TextInputInput({ className, ...props }: TextInputInputProps) {
+  return (
+    <input
+      className="bg-[transparent] flex-1 border-none outline-none text-gray-100 text-sm placeholder:text-gray-400"
+      {...props}
+    />
+  );
+}
+TextInputInput.displayName = "TextInput.Input";
+
+TextInput.Input = TextInputInput;
+TextInput.Icon = TextInputIcon;
